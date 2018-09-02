1b349bd7c4f6d89fefa19a5c20ec665d97e9c7aa
Jackie Yeo <jackie@ambercreative.sg>
2018-09-02 21:51:09 +0800

    first commit

---
+++ app/Todo.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Todo extends Model
+{
+    protected $fillable = [
+        'text',
+        'finished',
+    ];
+
+    protected $casts = [
+        'finished' => 'boolean',
+    ];
+
+
+}
